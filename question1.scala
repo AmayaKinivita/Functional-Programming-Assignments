181e8daf64c0ca9e350caba425b156962554eac0
AmayaKinivita <65851165+AmayaKinivita@users.noreply.github.com>
2020-05-24 16:28:37 +0530

    Add files via upload

---
+++ question1.scala
@@ -0,0 +1,11 @@
+object question1 { 
+      
+   def main(args: Array[String]): Unit= { 
+      def fahrenheit(x: Double) = x*1.8 +32
+	val ans = fahrenheit(35.0)
+	println("Value in Fahrenheit = " +ans)
+ 
+       
+   }
+
+} 
---
+++ question2.scala
@@ -0,0 +1,12 @@
+object question2 { 
+      
+   def main(args: Array[String]): Unit= { 
+      def volume(r: Int): Double ={ (4/3)*(22/7)*r*r*r }
+
+	val ans = volume(5)
+	println("Volume of the sphere =" +ans)
+ 
+       
+   }
+
+} 
